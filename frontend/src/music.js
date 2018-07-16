a86e264b42b8723420d0c5d5842a8075ae0ec31e
smedlang <smedlang@gmail.com>
2018-07-16 00:27:13 -0700

    backbone of website

---
+++ frontend/src/music.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+
+export default class Music extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state={};
+  }
+
+  render(){
+    return (
+      <div style={{justifyContent: "center"}}> <h1> Testing Music!! </h1> </div>
+    )
+  }
+
+}
