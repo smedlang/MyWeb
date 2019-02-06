ae900b9535f220b9608b5e5a69f75cd2fbf08754
smedlang <smedlang@gmail.com>
2019-02-05 22:40:53 -0500

    more

--- frontend/src/projects.js
+++ frontend/src/projects.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Link } from 'react';
 import docshare from './docshare.png';
 
 export default class Projects extends Component {
