921aff1fe69d21c3879b1aa1b2a82a1f2062b453
smedlang <smedlang@gmail.com>
2019-02-05 22:33:03 -0500

    project 1

--- frontend/src/projects.js
+++ frontend/src/projects.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import docshare from './docshare.png';
 
 export default class Projects extends Component {
   constructor(props){
@@ -9,8 +10,24 @@ export default class Projects extends Component {
   render(){
     return (
       <div>
-        COMING SOON
+        <Project photo={beach} caption="Document sharing"/>
       </div>
     );
+  }
+}
+class Project extends Component{
+  constructor(props){
+    super(props);
+
+    this.state={
+
+    }
+  }
+
+  render(){
+    return (<div>
+      <img src={docshare}/>
+      {caption}
+    </div>)
   }
 }
