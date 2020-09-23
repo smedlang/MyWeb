170aefd92c6c0a0f422846423a98e484c39c0e77
Ubuntu <ubuntu@ip-172-31-84-179.ec2.internal>
2020-09-23 20:41:24 +0000

    some changes

--- frontend/src/aboutme.js
+++ frontend/src/aboutme.js
@@ -12,9 +12,7 @@ export default class AboutMe extends React.Component{
       <div>
       <br />
         <img src={profilePic} style={{margin: '50px', float: 'left', width: '400px',  borderRadius: "50%"}}/>
-        <div style={{fontFamily: '"Comic Sans MS", cursive, sans-serif', fontSize: '18pt', padding: '100px'}}> Hello! I'm Savi Medlang. I'm a developer, passionate about
-        technology and user experience. I'm currently a senior at Case Western Reserve University, studying Computer Science. <br/> <br/>
-        Apart from coding, you can find me in a dog park, in the kitchen cooking, or performing music in my band.
+        <div style={{fontFamily: '"Comic Sans MS", cursive, sans-serif', fontSize: '18pt', padding: '100px'}}> Hi, I'm Savi! I'm a master's student in the Department of Computer Science at the University of Southern California. This past summer, I worked at Genesys as a Machine Learning Engineer intern on the Security team. The previous summer, I also worked at Genesys as a Software Engineer intern on the DevOps team. I've had experience in many areas of computer science, but I am most interested in machine learning. I enjoy problem solving and learning new skills.  <br/> <br/>
        </div>
 
       </div>
